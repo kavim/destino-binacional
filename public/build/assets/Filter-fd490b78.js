8eb7dc309437b6613c1642894b6b21ca3fc2d97d
KevinMMedeiros <kevin@cambioreal.com>
2023-05-23 03:18:52 -0300

    npm build

---
+++ public/build/assets/Filter-fd490b78.js
@@ -0,0 +1,1 @@
+import{a as e}from"./app-6dd16c0e.js";import{S as r}from"./SelectInput-ba16fb9d.js";function o({category:a}){return e("div",{className:"w-full",children:e("div",{children:e(r,{placeholder:"Select",name:"category"})})})}export{o as default};
