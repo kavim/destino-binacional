149c98c2f90ce2d03ec92a36a925f4a177e3e54f
kavim <kevinmmti@gmail.com>
2024-08-24 04:56:54 -0300

    Google tag

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -26,6 +26,16 @@
         })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
     </script>
 
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-95ZBQW1Y5E"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-95ZBQW1Y5E');
+    </script>
+
     <!-- Scripts -->
     @routes
     @viteReactRefresh
