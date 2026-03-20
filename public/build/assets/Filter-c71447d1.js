708affc5c96db4927b81c7fbbb2af91822700f77
kevin M. Medeiros <kevinmmti@gmail.com>
2026-03-20 10:07:14 +0000

    npm run build

--- public/build/assets/Filter-c71447d1.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./app-9502d4ec.js";import{S as r}from"./SelectInput-86be104a.js";function o({category:a}){return e("div",{className:"w-full",children:e("div",{children:e(r,{placeholder:"Select",name:"category"})})})}export{o as default};
