da41218f34b6b7766dff48a04e9dd840e27aaeb6
Felipe Maciel <felipemaciel1501@hotmail.com>
2026-03-30 10:32:29 -0300

    feat: refatoração UI/UX do cabeçalho da página de eventos

--- app/Models/City.php
+++ app/Models/City.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class City extends Model
 {
     use HasFactory;
+    public function state()
+    {
+        return $this->belongsTo(State::class);
+    }
 }
