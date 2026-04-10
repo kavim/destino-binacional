479ce8911e64bdb415d0090d1be96e83f53e16bb
kevin M. Medeiros <kmmtmg@gmail.com>
2026-04-10 13:02:27 -0300

    refactor: remove registration functionality and related components

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -838,11 +838,6 @@
                     <a href="{{ route('login') }}"
                         className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log
                         in</a>
-
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}"
-                            className="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-                    @endif
                 @endauth
             </div>
         @endif
